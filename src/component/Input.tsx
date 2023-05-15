9eb7feed99949d07ccaf8e3b62e2f1259f6041fc
oemejia <omej915@gmail.com>
2023-05-15 18:34:28 -0500

    Rounded Input component, implemented useState

--- src/component/Input.tsx
+++ src/component/Input.tsx
@@ -1,5 +1,5 @@
 export function Input(props: React.ComponentPropsWithoutRef<"input">) {
   return (
-    <input {...props} type="text" className="border border-gray-800"></input>
+    <input {...props} type="text" className="rounded border border-gray-800 dark:text-gray-800 px-4 py-2 "></input>
   );
 }
--- src/pages/generate.tsx
+++ src/pages/generate.tsx
@@ -1,11 +1,14 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import { useState } from "react";
 import { FormGroup } from "~/component/FormGroup";
 import { Input } from "~/component/Input";
 import { api } from "~/utils/api";
 
 const GeneratePage: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const [form, setForm] = useState({
+    prompt: "",
+  });
 
   return (
     <>
@@ -17,10 +20,20 @@ const GeneratePage: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center">
         <form className="flex flex-col gap-4">
           <FormGroup>
-          <label>Prompt</label>
-          <Input></Input>
+            <label>Prompt</label>
+            <Input
+            value={form.prompt}
+              onChange={(e) =>
+                setForm((prev) => ({
+                  ...form,
+                  prompt: e.target.value,
+                }))
+              }
+            ></Input>
           </FormGroup>
-          <button className="rounded bg-blue-400 hover:bg-blue-500 px-4 py-2">Submit</button>
+          <button className="rounded bg-blue-400 px-4 py-2 hover:bg-blue-500">
+            Submit
+          </button>
         </form>
       </main>
     </>
